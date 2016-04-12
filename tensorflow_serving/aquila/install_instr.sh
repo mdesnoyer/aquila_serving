e9302d0dda9b5eddd82d9413e9b096c21df7fd95
David Lea <lea@neon-lab.com>
2016-04-11 18:06:07 -0700

    updated instructions

--- tensorflow_serving/aquila/install_instr.sh
+++ tensorflow_serving/aquila/install_instr.sh
@@ -2,10 +2,11 @@
 
 # Install various packages
 sudo apt-get update
-sudo apt-get upgrade -y # choose “install package maintainers version”
-sudo apt-get install -y build-essential curl libfreetype6-dev libpng12-dev libzmq3-dev pkg-config python-pip python-dev git python-numpy python-scipy swig software-properties-common  python-dev default-jdk zip zlib1g-dev
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential curl libfreetype6-dev libpng12-dev libzmq3-dev pkg-config python-pip python-dev git python-numpy python-scipy swig software-properties-common  python-dev default-jdk zip zlib1g-dev ipython
 
-# install gRPC systemwide
+# upgrade six & install gRPC systemwide
+sudo pip install --upgrade six
 sudo pip install grpcio
 
 # Blacklist Noveau which has some kind of conflict with the nvidia driver
@@ -55,10 +56,12 @@ sudo apt-get install oracle-java8-set-default
 
 # this helps deal with the ABSOFUCKINGLUTELY COLOSSAL space requirements
 # of bazel and tensorflow
+cd /mnt/tmp
 sudo mkdir /mnt/tmp
 sudo chmod 777 /mnt/tmp
 sudo rm -rf /tmp
 sudo ln -s /mnt/tmp /tmp
+# ^^^ might not be necessary
 
 # install Bazel
 git clone https://github.com/bazelbuild/bazel.git
@@ -75,18 +78,21 @@ export CUDA_HOME=/usr/local/cuda
 
 # install tensorflow / tensorflow serving
 cd
-git clone --recurse-submodules https://github.com/neon-lab/serving.git
-git checkout aquila_serving
-cd serving/tensorflow
+git clone --recurse-submodules https://github.com/neon-lab/aquila_serving.git
+cd aquila_serving/tensorflow
 # configure tensorflow; unofficial settings are necessary given the GRID compute cap of 3.0
 TF_UNOFFICIAL_SETTING=1 ./configure  # accept the defaults; build with gpu support; set the compute capacity to 3.0
 cd ..
-# build the whole source tree - this will take a bit
-bazel build tensorflow_serving/...
+bazel build tensorflow_serving/...  # build the whole source tree - this will take a bit
+
+
+# convert tensorflow into a pip repo
+bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+pip install /tmp/tensorflow_pkg/tensorflow-0.7.1-py2-none-linux_x86_64.whl
+
 
 # clone aquila
 git clone https://github.com/neon-lab/aquila.git
-
-# convert tensorflow into a pip repo
 
 
