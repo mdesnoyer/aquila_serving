7bd423412947a0e92e0a3e34cf78a21b7a88d22e
David Lea <lea@neon-lab.com>
2016-04-13 16:22:05 -0700

    induced a resize over the images (even though its not necessary) to ensure to exported model knows what size the images should be

--- tensorflow_serving/aquila/aquila_export.py
+++ tensorflow_serving/aquila/aquila_export.py
@@ -1,5 +1,8 @@
 """
 Export inception model given existing training checkpoints.
+
+NOTE:
+  Input images should be decoded JPEGs, of size 299 x 299 x 3 on the domain [0, 1]
 """
 
 import os.path
@@ -39,22 +42,7 @@ def export():
 
     # Input transformation.
     # TODO(b/27776734): Add batching support.
-    jpegs = tf.placeholder(tf.string, shape=(1))
-    image_buffer = tf.squeeze(jpegs, [0])
-    # Decode the string as an RGB JPEG.
-    # Note that the resulting image contains an unknown height and width
-    # that is set dynamically by decode_jpeg. In other words, the height
-    # and width of image is unknown at compile-time.
-    image = tf.image.decode_jpeg(image_buffer, channels=3)
-    # After this point, all image pixels reside in [0,1)
-    # until the very end, when they're rescaled to (-1, 1).  The various
-    # adjust_* ops all require this range for dtype float.
-    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-    image = tf.squeeze(image, [0])
-    # Aquila 1.0 does not do this, so it's going to be omitted.
-    # Finally, rescale to [-1,1] instead of [0, 1)
-    # image = tf.sub(image, 0.5)
-    # image = tf.mul(image, 2.0)
+    image = tf.placeholder(tf.float32, shape=(FLAGS.image_size, FLAGS.image_size, 3))
     images = tf.expand_dims(image, 0)
 
     # Run inference.
